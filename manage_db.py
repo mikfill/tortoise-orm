c3e72aae1f40dbd89bed40789e758e10ff82faaf
Mykola Filippenko <mykolafilippenko@gmail.com>
2023-04-25 22:07:49 +0200

    fix manage_db.py

--- manage_db.py
+++ manage_db.py
@@ -1,43 +1,38 @@
+import asyncio
+
 from tortoise import Tortoise, run_async
 
-from go_rest_parse import fetch_data
+from go_rest_parse import BASE_URL, fetch_data
 from orm_schema import Comments, Posts, Todos, Users
 
 
 async def init_database():
     await Tortoise.init(db_url="sqlite://db.db", modules={"models": ["__main__"]})
-
     await Tortoise.generate_schemas()
 
-    data = await fetch_data()
+    try:
+        data = await asyncio.wait_for(fetch_data(), timeout=10)
+    except asyncio.TimeoutError:
+        print(f"fetch data from {BASE_URL} took too long to execution.")
+        return
+
     users = data[0]["users"]
     posts = data[1]["posts"]
     comments = data[2]["comments"]
     todos = data[3]["todos"]
 
-    del data
+    user_objs = [Users(**user_data) for user_data in users]
+    post_objs = [Posts(**post_data) for post_data in posts]
+    comment_objs = [Comments(**comment_data) for comment_data in comments]
+    todo_objs = [Todos(**todo_data) for todo_data in todos]
 
-    for user_data in users:
-        user = await Users(**user_data)
-        print(f"Create user: {user}")
-        await user.save()
-
-    for post_data in posts:
-        post = await Posts(**post_data)
-        print(f"Create post: {post}")
-        await post.save()
-
-    for comments_data in comments:
-        comment = await Comments(**comments_data)
-        print(f"Create comment: {comment}")
-        await comment.save()
-
-    for todos_data in todos:
-        todo = await Todos(**todos_data)
-        print(f"Create todo: {todo}")
-        await todo.save()
+    await asyncio.gather(
+        Users.bulk_create(user_objs),
+        Posts.bulk_create(post_objs),
+        Comments.bulk_create(comment_objs),
+        Todos.bulk_create(todo_objs)
+    )
 
     await Tortoise.close_connections()
 
-
-run_async(init_database())
+asyncio.run(init_database())
