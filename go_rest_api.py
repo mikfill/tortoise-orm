09e17ccda39c2c8285d8cf4da85122d5c5ef432c
Mykola Filippenko <mykolafilippenko@gmail.com>
2023-06-17 15:32:41 +0200

    run black and flake8

--- go_rest_api.py
+++ go_rest_api.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from tortoise import fields
-from tortoise.models import Model
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 
